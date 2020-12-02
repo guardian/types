999796c7eac6cff3a35ce67e7b4cf3c73322071b
Alex Sanders <a@snd.rs>
2020-12-02 17:19:35 +0000

    lint

--- src/index.ts
+++ src/index.ts
@@ -4,42 +4,40 @@ export type { Format } from './format';
 export { Pillar, Special, Theme, Design, Display } from './format';
 
 export type {
-    OphanABEvent,
-    OphanABPayload,
-    OphanAction,
-    OphanComponent,
-    OphanComponentEvent,
-    OphanComponentType,
-    OphanProduct,
-    TestMeta,
+	OphanABEvent,
+	OphanABPayload,
+	OphanAction,
+	OphanComponent,
+	OphanComponentEvent,
+	OphanComponentType,
+	OphanProduct,
+	TestMeta,
 } from './ophan';
 
-export {
-    Role,
-} from './role';
+export { Role } from './role';
 
 export type { Option } from './option';
 export {
-    OptionKind,
-    some,
-    none,
-    fromNullable,
-    withDefault,
-    map,
-    map2,
-    andThen,
+	OptionKind,
+	some,
+	none,
+	fromNullable,
+	withDefault,
+	map,
+	map2,
+	andThen,
 } from './option';
 
 export type { Result } from './result';
 export {
-    ResultKind,
-    ok,
-    err,
-    fromUnsafe,
-    partition,
-    either,
-    mapError,
-    toOption,
-    map as resultMap,
-    andThen as resultAndThen,
+	ResultKind,
+	ok,
+	err,
+	fromUnsafe,
+	partition,
+	either,
+	mapError,
+	toOption,
+	map as resultMap,
+	andThen as resultAndThen,
 } from './result';
